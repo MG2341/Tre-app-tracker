a8159ef3784072cc3cdda67847b4047bdab3be23
Matan Goldfarb <mgoldfarb90@gmail.com>
2026-06-23 14:47:41 +0300

    removed the test repo - no need for it

--- desktop/test/UseCaseTest.java
+++ desktop/test/UseCaseTest.java
@@ -9,32 +9,9 @@ import core.application.SessionService;
 import desktop.infrastructure.CsvLogRepositoryImpl;
 import desktop.ui.TreAppGui;
 
-
-/**
- * Mock implementation of LogRepository for testing
- */
-class InMemoryLogRepository implements LogRepository {
-    private List<SessionLog> logs = new ArrayList<>();
-
-    @Override
-    public void saveLog(SessionLog log) {
-        logs.add(log);
-    }
-
-    @Override
-    public boolean deleteLogById(String id) {
-        return logs.removeIf(log -> log.getId().equals(id));
-    }
-
-    @Override
-    public List<SessionLog> getAllLogs() {
-        return new ArrayList<>(logs);
-    }
-}
-
 /**
  * Test GUI launcher
- * Run this class with: java core.test.UseCaseTest
+ * Run this class with: java desktop.test.UseCaseTest
  */
 public class UseCaseTest {
     public static void main(String[] args) {
